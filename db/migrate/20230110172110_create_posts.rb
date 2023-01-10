84834076a97d7d40fb2bc915d15dda676e008bf1
EC2 Default User <ec2-user@ip-172-31-25-204.ap-northeast-1.compute.internal>
2023-01-10 17:44:07 +0000

    teble confit

--- db/migrate/20230110172110_create_posts.rb
+++ db/migrate/20230110172110_create_posts.rb
@@ -1,6 +1,13 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
+      
+      t.integer :user_id, null: false
+      t.integer :search_tag_id, null: false
+      t.string :titl, null: false
+      t.text :boby, null: false
+      
+      
 
       t.timestamps
     end
--- db/migrate/20230110172450_create_search_tags.rb
+++ db/migrate/20230110172450_create_search_tags.rb
@@ -1,6 +1,9 @@
 class CreateSearchTags < ActiveRecord::Migration[6.1]
   def change
     create_table :search_tags do |t|
+      
+      t.integer :tag_id, null: false
+      t.integer :post_id, null: false
 
       t.timestamps
     end
