eb8b3cfd37b074a6f9985fa732ad59ef651e3d1d
EC2 Default User <ec2-user@ip-172-31-25-204.ap-northeast-1.compute.internal>
2023-01-10 17:10:02 +0000

    routing config

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,43 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
   sessions: "admin/sessions"
 }
-  
-  
-  devise_for :user, skip: [:passwords], controllers: {
+
+  namespace :admin do
+
+     get '/' => 'homes#top'
+     
+     get '/search', to: 'searches#search'
+
+     resources :users, only: [:index, :show]
+
+     resources :posts, only: [:index, :show] do
+       resources :post_comments, only: [:destroy]
+      end
+     
+  end
+
+
+
+
+  devise_for :users, skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
+
+  scope module: :public do
+
+     get root to: 'homes#top'
+     
+     get '/search', to: 'searches#search'
+
+     resources :users, only: [:index, :show, :edit, :update, :destroy]
+
+     resources :posts do
+       resource :favorites, only: [:create, :destroy]
+       resources :post_comments, only: [:create, :destroy]
+     end
+
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
