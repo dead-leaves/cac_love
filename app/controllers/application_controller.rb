66a4d6a313fbf17aa1f4457b2e4ca50195236588
EC2 Default User <ec2-user@ip-172-31-25-204.ap-northeast-1.compute.internal>
2023-01-11 14:20:20 +0000

    bootstrap install

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,33 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  
+   def after_sign_in_path_for(resource)
+
+    case resource
+
+    when Admin
+      admin_path
+
+    when User
+      root_path
+    end
+
+   end
+
+  def after_sign_out_path_for(resource)
+
+    case resource
+
+    when :admin
+      admin_path
+
+    when :user
+        root_path
+    end
+
+  end
+
 
   protected
 
--- test/controllers/public/searches_controller_test.rb
+++ test/controllers/public/searches_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class Public::SearchsControllerTest < ActionDispatch::IntegrationTest
+class Public::SearchesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
